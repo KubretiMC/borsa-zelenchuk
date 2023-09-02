189b86ea0eae58e66424b6ae6be6a68f2442f7fe
Mariyan Sokolov <91256955+MarianSokolov@users.noreply.github.com>
2023-09-02 16:19:29 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/screens/**/*.{js,jsx,ts,tsx}", 
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkerTurquoise: '#54B6B6',
+        lighterTurquoise: '#A5D1CC',
+        customGray: '#405151',
+      },
+    },
+  },
+  plugins: [],
+}
+
