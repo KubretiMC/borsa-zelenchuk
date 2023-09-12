63abbb53ab76501443b71efa8e839680c321b651
Mariyan <marian.sokolov1@gmail.com>
2023-09-13 00:56:33 +0300

    modal warning fixed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
+ReactModal.setAppElement('#root');
+
 interface ModalProps {
   isOpen: boolean;
   text: string;
@@ -11,7 +13,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, text }) => {
     <ReactModal
       isOpen={isOpen}
       contentLabel="Success Modal"
-      className="bg-gray-200 rounded-lg p-4 mx-auto max-w-md w-full flex flex-col items-center justify-center mx-10"
+      className="bg-gray-200 rounded-lg p-4 mx-auto max-w-md w-full flex flex-col items-center justify-center mx-12"
       overlayClassName="fixed inset-0 flex items-center justify-center bg-opacity-50 bg-gray-700"
     >
       <h2 className="text-xl font-bold text-blue-800 text-center">{text}</h2>
