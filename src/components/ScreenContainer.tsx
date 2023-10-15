03b70d52c6d677ebdacb0cf1df72255c2ce51edb
Mariyan <marian.sokolov1@gmail.com>
2023-10-16 02:03:52 +0300

    fixed logic

--- src/components/ScreenContainer.tsx
+++ src/components/ScreenContainer.tsx
@@ -1,6 +1,14 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import Title from './Title';
 import LanguageSwitcher from './LanguageSwitcher';
+import _isEqual from 'lodash/isEqual';
+import { useDispatch, useSelector } from 'react-redux';
+import { DecodedToken, RootState } from '../interfaces/interfaces';
+import jwtDecode from 'jwt-decode';
+import { useNavigate } from 'react-router';
+import { fetchProducts, fetchUsers } from '../redux/actions';
+import Modal from './Modal';
+import { useTranslation } from 'react-i18next';
 
 interface ScreenContainerProps {
   children: ReactNode;
@@ -9,6 +17,92 @@ interface ScreenContainerProps {
 }
 
 const ScreenContainer: React.FC<ScreenContainerProps> = ({ subtitle, children, backButton }) => {
+  const { t } = useTranslation();
+  const loggedUser = useSelector((state: RootState) => state.loggedUser);
+  const token = localStorage.getItem('authToken');
+  const [modalData, setModalData] = useState<{ isOpen: boolean; text: string }>({ isOpen: false, text: '' });
+  const loggedUserRef = useRef({
+    id: '',
+    username: '',
+    phoneNumber: ''
+  });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!_isEqual(loggedUserRef.current, loggedUser)) {
+      if(loggedUser) {
+        loggedUserRef.current = loggedUser; 
+      }
+      const checkTokenExpiration = () => {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+          const decodedToken: DecodedToken = jwtDecode(token);
+          const currentTime = Math.floor(Date.now() / 1000);
+    
+          if (decodedToken.exp < currentTime) {
+            setModalData({ isOpen: true, text: t('SESSION_EXPIRED') });
+            setTimeout(() =>{
+              navigate('/');
+            }, 2000)
+          } else {
+            return decodedToken.userId;
+          }
+        } else {
+          setModalData({ isOpen: true, text: t('ERROR_OCCURED') });
+          setTimeout(() =>{
+            navigate('/');
+          }, 2000)
+        }
+      }
+
+      const userId = checkTokenExpiration();
+      if(userId) {
+        const apiUrl = process.env.REACT_APP_API_URL;   
+        
+        const getAllUsers = async () => {
+          try {
+            const response = await fetch(`${apiUrl}/user/getAllUsers`, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+              },
+            });
+
+            if (response.ok) {
+              const data = await response.json();
+              dispatch(fetchUsers(data, userId));
+            }
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        }
+
+        const getAllProducts = async () => {
+          try { 
+            const response = await fetch(`${apiUrl}/product/getAllProducts`, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+              },
+            });
+
+            if (response.ok) {
+              const data = await response.json();
+              dispatch(fetchProducts(data));
+            };
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        }
+
+        getAllUsers();
+        getAllProducts();
+      }
+    }
+  }, [loggedUser, dispatch, navigate, t, token]);
   return (
     <div className="mainWrapper">
       <div className="wrapper">
@@ -18,6 +112,9 @@ const ScreenContainer: React.FC<ScreenContainerProps> = ({ subtitle, children, b
           {children}
         </div>
       </div>
+      <Modal isOpen={modalData.isOpen}>
+          <h2 className="text-xl font-bold text-blue-800 text-center">{modalData.text}</h2>
+      </Modal>
     </div>
   );
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
-import { DecodedToken, RootState } from '../interfaces/interfaces';
-import { fetchProductFilters, fetchProducts, fetchUsers, logoutUser } from '../redux/actions';
+import { RootState } from '../interfaces/interfaces';
+import { fetchProductFilters, logoutUser } from '../redux/actions';
 import ScreenContainer from '../components/ScreenContainer';
 import Button from '../components/Button';
-import jwtDecode from 'jwt-decode';
 import _isEqual from 'lodash/isEqual';
-import Modal from '../components/Modal';
 
 const HomeScreen: React.FC = () => {
   const { t } = useTranslation();
@@ -18,12 +16,6 @@ const HomeScreen: React.FC = () => {
   const loggedUser = useSelector((state: RootState) => state.loggedUser);
 
   const token = localStorage.getItem('authToken');
-  const [modalData, setModalData] = useState<{ isOpen: boolean; text: string }>({ isOpen: false, text: '' });
-  const loggedUserRef = useRef({
-    id: '',
-    username: '',
-    phoneNumber: ''
-  });
 
   const handleNavigateButtonClick = (path: string) => {
     navigate(path);
@@ -35,74 +27,8 @@ const HomeScreen: React.FC = () => {
   }
   
   useEffect(() => {
-    if (!_isEqual(loggedUserRef.current, loggedUser)) {
-      if(loggedUser) {
-        loggedUserRef.current = loggedUser; 
-      }
-      const checkTokenExpiration = () => {
-        const token = localStorage.getItem('authToken');
-        if (token) {
-          const decodedToken: DecodedToken = jwtDecode(token);
-          const currentTime = Math.floor(Date.now() / 1000);
-    
-          if (decodedToken.exp < currentTime) {
-            setModalData({ isOpen: true, text: t('SESSION_EXPIRED') });
-            setTimeout(() =>{
-              navigate('/');
-            }, 2000)
-          } else {
-            return decodedToken.userId;
-          }
-        } else {
-          setModalData({ isOpen: true, text: t('ERROR_OCCURED') });
-          setTimeout(() =>{
-            navigate('/');
-          }, 2000)
-        }
-      }
-
-      const userId = checkTokenExpiration();
-      if(userId) {
+    if (!loggedUser) {
         const apiUrl = process.env.REACT_APP_API_URL;   
-        
-        const getAllUsers = async () => {
-          try {
-            const response = await fetch(`${apiUrl}/user/getAllUsers`, {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-              },
-            });
-
-            if (response.ok) {
-              const data = await response.json();
-              dispatch(fetchUsers(data, userId));
-            }
-          } catch (error) {
-            console.error('Error:', error);
-          }
-        }
-
-        const getAllProducts = async () => {
-          try { 
-            const response = await fetch(`${apiUrl}/product/getAllProducts`, {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-              },
-            });
-
-            if (response.ok) {
-              const data = await response.json();
-              dispatch(fetchProducts(data));
-            };
-          } catch (error) {
-            console.error('Error:', error);
-          }
-        }
-
         const getAllProductFilters = async () => {
           try { 
             const response = await fetch(`${apiUrl}/productFilters/getProductFilters`, {
@@ -121,13 +47,9 @@ const HomeScreen: React.FC = () => {
             console.error('Error:', error);
           }
         }
-
-        getAllUsers();
-        getAllProducts();
         getAllProductFilters();
-      }
     }
-  }, [loggedUser, dispatch, navigate, t, token]);
+  }, [loggedUser, dispatch, t, token]);
 
   return (
     <ScreenContainer subtitle={loggedUser?.username || ''}>
@@ -139,9 +61,6 @@ const HomeScreen: React.FC = () => {
         <Button title={t('OFFER_MAKE')} onClick={() => handleNavigateButtonClick('/add-offer')} />
         <Button title={t('LOGOUT')} onClick={() => handleLogout()} />
       </div>
-      <Modal isOpen={modalData.isOpen}>
-          <h2 className="text-xl font-bold text-blue-800 text-center">{modalData.text}</h2>
-      </Modal>
     </ScreenContainer>
   );
 };
