378d697046ac44cce433db5b8efe8c083ca3dfbe
Mariyan <marian.sokolov1@gmail.com>
2023-10-16 01:44:33 +0300

    removed unused code

--- src/components/ScreenContainer.tsx
+++ src/components/ScreenContainer.tsx
@@ -1,13 +1,6 @@
-import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import React, { ReactNode } from 'react';
 import Title from './Title';
 import LanguageSwitcher from './LanguageSwitcher';
-import { fetchProductFilters, fetchProducts, fetchUsers } from '../redux/actions';
-import { useDispatch, useSelector } from 'react-redux';
-import { DecodedToken, RootState } from '../interfaces/interfaces';
-import jwtDecode from 'jwt-decode';
-import Modal from './Modal';
-import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 import _isEqual from 'lodash/isEqual';
 
 interface ScreenContainerProps {
